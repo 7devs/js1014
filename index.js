68b120972b06fbe669979939a5a47cbf17efeca4
hellojiangshan <jiangshan2010@vip.sina.com>
2016-10-14 20:34:59 +0800

    new

---
+++ index.js
@@ -0,0 +1,22 @@
+var app = require('express')();
+var bodyParser = require('body-parser');
+var wechat = require('./lib/wechat');
+var config = require('./lib/config');
+
+app.use(bodyParser.urlencoded({
+  extended: false,
+}));
+
+wechat(config.wechat);
+
+wechat.createMenu(require('./lib/menu.json'));
+
+app.use('/wxapi', require('./lib/routers/wxapi.js'));
+
+app.get('/', function(req, res, next) {
+  res.send('1474902499322');
+});
+
+app.listen(8012, function(error) {
+  console.log('listenning at 8012...');
+});
